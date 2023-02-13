04914f62cf75232e3628837d16eb9637f81182a0
Liefe Sutliff <lsutliff@student.cvtc.edu>
2023-02-13 16:16:41 -0600

    Updated files

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5163
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .use(express.json())
+  .use(express.urlencoded({ extended: true }))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', function (req, res) {
+    const ejsData = {
+
+    }
+    console.log(ejsData)
+    res.render('pages/index', ejsData)
+  })
+  .get('/health', function (req, res) {
+    res.status(200).send('healthy')
+  })
+  .get('/about', function (req, res) {
+    const ejsData = {
+
+    }
+    console.log(ejsData)
+    res.render('pages/about', ejsData)
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))

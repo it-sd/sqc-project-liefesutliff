2ce79773da238f9aa81a679714f4bf558a983420
liefesutliff <123669909+liefesutliff@users.noreply.github.com>
2023-03-13 23:38:25 -0500

    Revert "Added star email functionality"

--- spec/support/clientSpec.js
+++ spec/support/clientSpec.js
@@ -1,10 +1,5 @@
 const { Builder, By } = require('selenium-webdriver')
 
-describe('Passwords client', function () {
-  const baseUrl = 'http://localhost:5163/'
-  let driver
-
-  describe("on '/'", function () {
 describe('client', function () {
   describe('email', function () {
     const baseUrl = 'http://localhost:5163/compose'
@@ -19,26 +14,6 @@ describe('client', function () {
       await driver.quit()
     })
 
-    it('should set the value in the star column equal to the opposite of what it is', async function () {
-      const closedDiv = await driver.findElement(By.id('closed')).getRect()
-      const star = await driver.findElement(By.id('star'))
-      const action = driver.actions({ async: true })
-
-      await action.move({ x: closedDiv.x + 1, y: closedDiv.y + 1 }).click().perform()
-
-      const oldDisplay = await star.getText()
-
-      await action.move({ origin: star }).click().pause(1000).perform()
-
-      await driver.actions()
-
-      const display = await star.getText()
-
-      if (oldDisplay === 'Star Email') {
-        expect(display).toBe('Starred Email')
-      } else {
-        expect(display).toBe('Star Email')
-      }
     it('should contain recipient', async function () {
       const details = await driver.findElement(By.id('recipient'))
       expect(details).toBeDefined()
